38e6ae132af881d30cab2668a2f0fbd14df0221a
ggavilan468 <83104070+ggavilan468@users.noreply.github.com>
2023-11-09 11:43:31 -0800

    Update DSM3Dqrf.R
    final

--- DSM3Dqrf.R
+++ DSM3Dqrf.R
@@ -1,10 +1,25 @@
-#The data includes information on silt, sand, clay, and organic matter (OM) collected from soil pits, 
-#with details on their respective depths (multiple samples were taken at different depths per soil pit)
+#The data in "dat" includes information on silt, sand, clay, and soil organic matter (SOM) collected from soil pits, 
+#with details on their respective depths (multiple samples were taken at different depths per soil pit), and the SCORPAN variables
 
 library(quantregForest)
 library(caret)
 library(doParallel)
 
+#example for SOM
+# Sd and percentile predictions
+
+# Search for the best mtry parameter
+rfmodel <- train(fm2, data = dat, method = "qrf", trControl = controlmtry, importance = TRUE)
+
+SOMmodel <- quantregForest(x=dat[,c("DEPTH_g","biovars.1","biovars.12","biovars.2","Channel_Network_Base_Level",
+                                    "biovars.4","dem","Slope","Diffuse_Insolation","MRVBF")],y=dat$mo, nthreads=25, keep.inbag=FALSE, mtry=3)
+
+#percentile prediction
+predict(object = SOMmodel, newdata=framecovs, what=c(0.1,0.25,0.5,0.75,0.9))
+#standard deviation prediction
+predict(object = SOMmodel, newdata=framecovs, what=sd)
+
+#Example for 3d SOM representation
 fm2 = as.formula(paste0("MO ~ DEPTH +",  paste(topvariables,
                                                   collapse = "+")))
 # Search for the best mtry parameter
